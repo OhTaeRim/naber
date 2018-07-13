aac24e5876da9559bbe65b0b6bdc145a7eeae79c
troh <dhxofla123@naver.com>
2018-07-13 22:21:34 +0900

    first commit

---
+++ naber/src/main/java/com/troh/naber/NaberApplication.java
@@ -0,0 +1,12 @@
+package com.troh.naber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaberApplication.class, args);
+	}
+}
